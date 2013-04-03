1e4094f0d9e8dd535d6b16aa66dd604456e0d943
Ray Wadkins <raymond.wadkins@tdameritrade.com>
2013-04-03 16:46:51 -0400

    Adding support for ImageMagick auto install script

---
+++ scripts/imagemagick.js
@@ -0,0 +1,37 @@
+var spawn = require('child_process').spawn, 
+	shell, 
+	platform = process.platform, 
+	exec = 'default';
+	
+var dispatch = {
+	win32 : function() {
+		handleExec(null, "\nPlease install ImageMagick (http://www.imagemagick.org)\n", null); 
+	},
+	default : function() {
+		shell = spawn.execFile('./imagemagick.sh', handleExec);
+	}
+}
+
+if (platform in dispatch) {
+	exec = platform;
+}
+
+dispatch[exec].call();
+
+function handleExec(error, stdOut, stdErr) {
+	var exit = 0;
+	
+	if (stdErr) {
+		console.error(stdErr);
+	}
+	
+	if (stdOut) {
+		console.log(stdOut);
+	}
+	
+	if (error) {
+		exit = error.code;
+	}
+	
+	process.exit(exit);
+}
