9b7e544a90146be686301a90b7f8c4b3f6055b8b
Lance Vick <lvick@accesso.com>
2013-04-10 13:28:02 -0400

    minor changes to to support grunt 4.0

--- tasks/spritesheet.js
+++ tasks/spritesheet.js
@@ -3,14 +3,10 @@ module.exports = function(grunt) {"use strict";
     var Builder = require('../').Builder;
 
     grunt.registerMultiTask("spritesheet", "Compile images to sprite sheet", function() {
-        var helpers = require('grunt-lib-contrib').init(grunt);
-        var options = helpers.options(this);
+        var options = this.options(this);
         var done = this.async()
 
         grunt.verbose.writeflags(options, "Options");
-
-        // TODO: ditch this when grunt v0.4 is released
-        this.files = this.files || helpers.normalizeMultiTaskFiles(this.data, this.target);
 
         var srcFiles;
         var images;
@@ -33,4 +29,4 @@ module.exports = function(grunt) {"use strict";
             builder.build(callback);
         }, done);
     });
-}; 
+};
